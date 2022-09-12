f9aa25671bc741287b12420b0aa0926321ce5528
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-09-13 03:02:47 +0500

    Fix register, sign in and header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,86 +1,79 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import { FC } from "react";  
+import Menu from '@mui/material/Menu';
+import { FC } from "react"; 
+import Link from '@mui/material/Link';
+import AccountCircle from '@mui/icons-material/AccountCircle'; 
 
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+//const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Header: FC = () => {
-  
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+ 
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters sx={{justifyContent: "space-between"}}>          
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-              fontWeight: 700,              
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Invoice Management System
+        <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >            
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
           </Typography>
-
-          <Box>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}><Link href="#" underline="none" color="#141414">Profile</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link href="#" underline="none" color="#141414">My Account</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link href="#" underline="none" color="#141414">Logout</Link></MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Container from '@mui/material/Container';
 import { CookiesProvider } from "react-cookie";
 import { Provider } from 'react-redux'
 import { store } from './store/store';
@@ -16,11 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
      <Provider store={store}>
-      <CookiesProvider>
-        <Container fixed>
+      <CookiesProvider>        
         <ToastContainer /> 
-          <App />      
-        </Container>
+          <App />
       </CookiesProvider>
     </Provider>
   </React.StrictMode>
