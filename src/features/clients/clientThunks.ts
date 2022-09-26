0e492f611846f51a5834e904ff2a35dc9a3654cb
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-09-27 04:08:21 +0500

    worked on client page

--- src/features/clients/clientThunks.ts
+++ src/features/clients/clientThunks.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { GraphQLClient, gql } from 'graphql-request'
+
 export const getClients = async (token: string) => {
   const url = "http://localhost:3139/clients";
   const response = await axios.get(url, {
@@ -42,3 +44,30 @@ export const getSingleClient = async (clientId: string, token: string) => {
   });
   return response;
 };
+
+
+export const graphqlGetClients = async (token: string, sortOrder: string = 'asc', sortBy: string = 'creation', limit: string = '10', offset: string = '0' ) => {
+  const endpoint = 'http://localhost:3139/graphql';
+ 
+  const graphQLClient = new GraphQLClient(endpoint, {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    jsonSerializer: {
+      parse: JSON.parse,
+      stringify: JSON.stringify,
+    },
+  })
+
+  const query = gql`
+  {
+    clients(sort: {${sortBy} : ${sortOrder}}, limit: ${limit}, offset: ${offset}) {
+      results {id name email invoicesCount totalBilled companyDetails {name}} total
+    }
+  }
+  `;
+
+ return await graphQLClient.request(query)
+}
