38a6d177e2ebec806010962a55c6ef70cf59d22d
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-09-29 01:13:47 +0500

    worked on create and edit funcationality

--- src/features/invoices/invoiceThunks.ts
+++ src/features/invoices/invoiceThunks.ts
@@ -11,10 +11,31 @@ export const getInvoices = async (token: string, filters: any) => {
   return response;
 };
 
-
 export const createInvoice = async (data: any, token: string) => {
   const url = "http://localhost:3139/invoices";
   const response = await axios.post(url, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response;
+};
+
+export const getSingleInvoice = async (token: string, id: string) => {
+  const url = `http://localhost:3139/invoices/${id}`;
+  const response = await axios.get(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  });
+  return response;
+};
+
+export const updateInvoice = async (data: any, token: string) => {
+  const url = "http://localhost:3139/invoices";
+  const response = await axios.put(url, data, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
